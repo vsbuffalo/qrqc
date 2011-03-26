d550da07af32c631cf23a15bf7dc56f7e92129b7
Vince Buffalo <vsbuffalo@gmail.com>
2011-03-26 12:20:51 -0700

    fixed documentation issues and inconsistencies

--- Makefile
+++ Makefile
@@ -5,17 +5,22 @@
 DIR=qrqc
 PKG=$(DIR)_*.tar.gz
 
-build: clean
-	R CMD build $(DIR)
+build: clean check
+	R CMD BUILD $(DIR)
+
+check: check
+	R CMD CHECK $(DIR)
+
 
 install:
-	R CMD install $(PKG)
+	R CMD INSTALL $(PKG)
 
 all: build install
 
 clean: clean-pdf
 	rm -f qrqc_*.tar.gz
 	rm -rf qrqc/inst/doc/auto
+	rm -f qrqc/src/*.o qrqc/src/*.so
 
 clean-pdf:
 	rm -f $(DIR).pdf
--- qrqc/R/AllGenerics.R
+++ qrqc/R/AllGenerics.R
@@ -4,4 +4,4 @@ setGeneric("plotQuals", signature="obj", function(obj, ylim='relative', lowess=T
 setGeneric("plotBases", signature="obj", function(obj, type="freq", bases=NULL, legend=TRUE) standardGeneric("plotBases"))
 setGeneric("plotSeqLengths", signature="obj", function(obj) standardGeneric("plotSeqLengths"))
 setGeneric("plotGC", signature="obj", function(obj) standardGeneric("plotGC"))
-setGeneric("makeReport", signature="obj", function(obj, outputDir=NULL) standardGeneric("makeReport"))
+setGeneric("makeReport", signature="obj", function(obj, outputDir=".") standardGeneric("makeReport"))
